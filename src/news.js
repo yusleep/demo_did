5cc38df3942c84afb53c6e8ae74657a48053471a
Yusleep <3309484330@qq.com>
2023-12-07 15:17:42 +0800

    Update news.js

--- src/news.js
+++ src/news.js
@@ -88,3 +88,69 @@ button.addEventListener("click", function () {
       console.error("Error fetching message:", error);
     });
 });
+
+const buttonVerify = document.getElementById("verifyMessageButton");
+
+buttonVerify.addEventListener("click", function () {
+  // Using the Fetch API to send a GET request to a server
+  fetch("http://localhost:8080")
+    .then((response) => response.json())
+    .then((data) => {
+      // Storing the returned value from the server
+      const messageFromServer = data.message;
+
+      const buttonSideDiv = document.getElementById("button-side");
+
+      // 设置新的HTML内容
+      const newContent = `
+      
+      <div class="header-sign">
+        <h1>Verify request</h1>
+        <p>You can verify the website with</p>
+      </div>
+      <div class="message-box">
+        <p><strong>Message:</strong></p>
+        <p>Sign in with IX-Chain</p>
+        <p><strong>URI:</strong></p>
+        <p>https://dailynews.com</p>
+        <p><strong>Signature:</strong></p>
+        <p>1</p>
+      </div>
+      <div class="button-container">
+        <button class="cancel-button" id="cancelSignButton">Cancel</button>
+        <button class="sign-button" id="signMessageButton">Verify</button>
+      </div>
+   
+    `;
+
+      // 更新button-side div的内容
+      buttonSideDiv.innerHTML = newContent;
+
+      // Now that the signatureButton exists in the DOM, we can safely add an event listener to it
+
+      document
+        .getElementById("signMessageButton")
+        .addEventListener("click", function () {
+          alert("Successful!");
+          window.location.href =
+            "file:///Users/fishbook/MyCode/demo_did/src/news_sign.html";
+        });
+
+      document
+        .getElementById("cancelSignButton")
+        .addEventListener("click", function () {
+          window.location.href =
+            "file:///Users/fishbook/MyCode/demo_did/src/news_sign.html";
+        });
+
+      document
+        .getElementById("signatureButton")
+        .addEventListener("click", function () {
+          // Handle the signature button click
+          // Place your code here that should run when the signature button is clicked
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching message:", error);
+    });
+});
